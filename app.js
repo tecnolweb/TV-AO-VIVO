87b2950e6773e67a15ac77446f38a325260f3afb
gilberto <gilbertomoral61@gmail.com>
2025-11-26 20:59:21 -0300

    app

--- app.js
+++ app.js
@@ -2,12 +2,13 @@
 // LISTA DE CANAIS
 // ===============================
 const canais = [
-    { nome: "Globo", imagem: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Globo_logo.png", url: "https://live-ber-sp-05-07.video.globo.com/j/eyJhbGciOiJSUzUxMiIsImtpZCI6IjEiLCJ0eXAiOiJKV1QifQ.eyJjb3VudHJ5X2NvZGUiOiJCUiIsImRvbWFpbiI6ImxpdmUtYmVyLXNwLTA1LTA3LnZpZGVvLmdsb2JvLmNvbSIsImV4cCI6MTc2NDE0MDI2NSwiaWF0IjoxNzY0MTE4NjY1LCJpc3MiOiJwbGF5YmFjay1hcGktcHJvZC1nY3AiLCJvd25lciI6ImQ4N2VjOGNkLWU2ZjQtNGFiMC1hY2ZiLTg2NzJhYzFlYzc2YSIsInBhdGgiOiIvbGl2ZS9mKGk9MikvZ2xvYm8tY3JyL3BsYXlsaXN0Lm0zdTgifQ.eaOTeNniFoFPY28dsTXOmLSDGn254BDYbwa47YmFhYoiJhpBbwPFwg-zLVQVQSYT7N-k4nnKxxpausl3dEtxvvvNy-kRBFj-DkoZXR49qOZ5GIyU1ZDuezGgD9_2sa2654F5jrryk9cq9EBu92wOZIxlTEpSL0cCyS6J0A20_2RgFCQyv2GU93Y8swPF5eLjTv6xLE-foZPgY0MPdD57wEAFmoZXkW5KMC3JpVXuhfADbkc5Ks3oF2SX6kdph8LgnPCxnL4JhsN0DzgcrsQH9w_iSLhMRyG6-yKEU32YFl0DYd3bcBMz3vPmcCi76Y_CcVnmnDypG5-A5rhvoN2c4w/live/f(i=2)/globo-crr/globo-crr-audio_1=96000-video=2262976.m3u8" },
+   
     { nome: "SBT", imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt7GWLf3UTvL3scCXnIe4EemT4ny_qsmomcw&s", url: "https://cdn.live.br1.jmvstream.com/w/LVW-9359/LVW9359_XSyReL0QVf/chunklist.m3u8" },
-    
     { nome: "Band", imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHXdT1tAeUWXYvybPIOtIN69KG4kg-YcLRtQ&s", url: "https://hqf6tcxuhk.singularcdn.net.br/live/019498af-1fdf-7df5-86bd-e4a6f588d6a7/s1/playlist-1080p.m3u8?sjwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmlfc3ViIjoiLzAxOTQ5OGFmLTFmZGYtN2RmNS04NmJkLWU0YTZmNTg4ZDZhNy8iLCJ1aWQiOiJhMjliYWJlOS00NjQzLTQ4NjUtYTQxMS04ZWUwODIzZWQwZDYiLCJyYXUiOm51bGwsImJleSI6ZmFsc2UsImlpcCI6ZmFsc2UsIm5iZiI6MTc2NDExNjAzNSwiaWF0IjoxNzY0MTE2MDM1LCJleHAiOjE3OTU2NTIwMzUsImp0aSI6ImEyOWJhYmU5LTQ2NDMtNDg2NS1hNDExLThlZTA4MjNlZDBkNiIsImlzcyI6IlNwYWxsYSJ9.B4lJ2PHWfDvtGEXKOvLLEj_bEyxBzU1wY5una2vfDJ4&uid=a29babe9-4643-4865-a411-8ee0823ed0d6" },
     { nome: "Record", imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgrqTv2fif4I4zzsIzbvyOpkbTq4nxHFEisA&s", url: "https://video08.logicahost.com.br/portaldatropical02/portaldatropical02/chunklist_w2097626994.m3u8" },
-    { nome: "RedeTV", imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlF2skYmRpVKIGI0Iy6uucUOxo-leHLCnEzQ&s", url: "https://video08.logicahost.com.br/portaldatropical02/portaldatropical02/chunklist_w2097626994.m3u8" }
+    { nome: "RedeTV", imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlF2skYmRpVKIGI0Iy6uucUOxo-leHLCnEzQ&s", url: "https://video08.logicahost.com.br/portaldatropical02/portaldatropical02/chunklist_w2097626994.m3u8" },
+    { nome: "TV Maná Brasil", imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMdWTezfdetPCYlmnrEGutx6VD4v9rrq362A&s", url: "https://w1.manasat.com/tvmana-brasil/smil:tvmana-brasil.smil/playlist.m3u8"},
+
 ];
 
 // SOM DE NAVEGAÇÃO
